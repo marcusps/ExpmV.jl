afc19876fa42a8152eb6932623c1152e86a349f1
Marcus P S <msilva@bbn.com>
2015-08-11 17:25:54 -0400

    Implemented two of Higham and Tisseur's algorithms, both appear to work.

--- src/normest1.jl
+++ src/normest1.jl
@@ -1,35 +1,66 @@
-function ht22(A,t::Int)
-    # TODO: if A is not nxn (square), error
+function ht21(A;itmax=20)
+    n,m = size(A)
+    if n!=m
+        error("norm 1 estimation can only be applied to square matrices")
+    end
+    x = ones(n)/n
+    γ = 0
+    itcount = itmax
+    for k in 1:itmax
+        y = A*x
+        ξ = sign(y)
+        z = A'*ξ
+        if norm(z,Inf)<=dot(z,x) && k>1
+            γ=norm(y,1)
+            itcount = k
+            break
+        end
+        x = zeros(Float64,n); x[indmax(abs(z))] = 1.
+    end
+    b = Float64[ (-1)^(i+1) * (1+(i-1)/(n-1)) for i in 1:n]
+    return max(γ,norm(A*b,1)/norm(b,1)), itcount
+end
+
+function ht22(A,t::Int;itmax=10,debug=false)
     n,m = size(A)
     if n!=m
         error("norm 1 estimation can only be applied to square matrices")
     end
     # start with a random matrix of normalized columns
-    X = randn(size(A,1),t)
+    X = randn(n,t)
     for col=1:t
         scale!(slice(X,1:n,col),1/norm(slice(X,1:n,col),1))
     end
     I = eye(n)
-    while true
+    h = Float64[]
+    itcount = itmax
+    for k in 1:itmax
         Y = A*X
         g = Float64[norm(Y[:,j],1) for j in 1:t]
-        _, ind_best = findmax(g)
-        sort!(g)
+        ind_best = indmax(g)
+        sort!(g,rev=true)
+        if debug
+            println(g[1])
+        end
         S = sign(Y)
         Z = A'*S
-        h = Float64[norm(Z[i,:],Inf) for i in 1:n]
+        h = Float64[maximum(abs(vec(Z[i,:]))) for i in 1:n]
         ind = [1:n;]
-        if maximum(h) <= Z[:,ind_best]'*X[:,ind_best]
+        h_ind = sortrows(hcat(h,ind),by=x->x[1],rev=true)
+        h   = h_ind[:,1]
+        ind = integer(h_ind[:,2])#round(Int,h_ind[:,2])
+        if maximum(h) <= dot(Z[:,ind_best],X[:,ind_best])
+            itcount = k
             break
         end
-        h_ind = sortrows(hcat(h,ind),by=x->x[1])
-        h   = h_ind[:,1]
-        ind = h_ind[:,2]
+        if debug
+            println(h[1])
+        end
         for j in 1:t
             X[:,j] = I[:,ind[j]]
         end
     end
-    return h
+    return h[1], itcount
 end
 
 function ht23(A,t::Int;itmax=2)
@@ -91,12 +122,19 @@ function ht23(A,t::Int;itmax=2)
   return est,
 end
 
-function test(k,d=10)
-    res = zeros(k,2)
-    for i in 1:k
-        A = randn(d,d)+1im*randn(d,d)
-        res[i,1] = norm(A,1)
-        res[i,2] = ht22(A,6)
-    end
-    res
+function test(k;d=10)
+    A = randn(d,d); 
+    return ht21(A), ht22(A,k), norm(A,1)
+#     res = zeros(k,5)
+#     for i in 1:k
+#         A = randn(d,d)
+#         res[i,1] = norm(A,1)
+#         est21, it21 = ht21(A)
+#         res[i,2] = est21
+#         res[i,3] = it21
+#         est22, it22 = ht22(A,d)
+#         res[i,4] = est22
+#         res[i,5] = it22
+#     end
+#     res
 end
