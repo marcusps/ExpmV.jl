483b1cf74533ee1f1923cad61a9f0adb52b92058
Marcus P S <marcus.ps@gmail.com>
2016-11-26 21:43:28 -0500

    More careful benchmarking at different dimensions

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -2,33 +2,20 @@ using BenchmarkTools, Expokit, ExpmV
 
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 120
 
-const d = 1_000
-const p = 1e-4
+const p = 1e-3
 
-b1 = @benchmarkable expm(full_r)*rv setup=((full_r,rv) = (full(randn()*sprandn(d,d,p/2)+1im*sprandn(d,d,p/2)), rv=normalize(randn(d)+1im*randn(d))))
-b2 = @benchmarkable Expokit.expmv(rt,r,rv) setup=((rt,r,rv)=(rand(), sprandn(d,d,p/2)+1im*sprandn(d,d,p/2), normalize(randn(d)+1im*randn(d))))
-b3 = @benchmarkable ExpmV.expmv(rt,r,rv) setup=((rt,r,rv)=(rand(), sprandn(d,d,p/2)+1im*sprandn(d,d,p/2), normalize(randn(d)+1im*randn(d))))
+bench = Dict{String,Any}[]
+for d in 2.^(5:10)
+    println("d = $d ...")
+    b1 = @benchmarkable expm(full_r)*rv setup=((full_r,rv) = (full(randn()*sprandn($d,$d,p/2)+1im*sprandn($d,$d,p/2)), rv=normalize(randn($d)+1im*randn($d))))
+    b2 = @benchmarkable Expokit.expmv(rt,r,rv) setup=((rt,r,rv)=(rand(), sprandn($d,$d,p/2)+1im*sprandn($d,$d,p/2), normalize(randn($d)+1im*randn($d))))
+    b3 = @benchmarkable ExpmV.expmv(rt,r,rv) setup=((rt,r,rv)=(rand(), sprandn($d,$d,p/2)+1im*sprandn($d,$d,p/2), normalize(randn($d)+1im*randn($d))))
 
-t1 = run(b1)
-t2 = run(b2)
-t3 = run(b3)
+    t1 = run(b1)
+    t2 = run(b2)
+    t3 = run(b3)
 
-println("Full expm")
-println(t1)
+    push!(bench,Dict("d"=>d, "p"=>p,"expm"=>t1,"expokit"=>t2,"expmv"=>t3))
+end
 
-println("Expokit")
-println(t2)
-
-println("ExpmV")
-println(t3)
-
-println("Full expm vs. Expokit")
-println(judge(median(t2),median(t1)))
-
-println("Full expm vs. ExpmV")
-println(judge(median(t3),median(t1)))
-
-println("Expokit vs. ExpmV")
-println(judge(median(t3),median(t2)))
-
-JLD.save("master-bench.jld","t1",t1,"t2",t2,"t3",t3)
+JLD.save("master-bench.jld","bench",bench)
