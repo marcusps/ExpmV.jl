1c6b3a4d14da77e5c0319dcaa66ccfff832aaed1
Matteo Rossi <teo.red90@gmail.com>
2018-05-02 18:46:35 +0300

    Suppressed another warning

--- src/normAm.jl
+++ src/normAm.jl
@@ -4,20 +4,20 @@ function normAm(A,m)
     #   If A has nonnegative elements the estimate is exact.
     #   [C,MV] = NORMAM(A,m) returns the estimate C and the number MV of
     #   matrix-vector products computed involving A or A^*.
-    
+
     #   Reference: A. H. Al-Mohy and N. J. Higham, A New Scaling and Squaring
     #   Algorithm for the Matrix Exponential, SIAM J. Matrix Anal. Appl. 31(3):
     #   970-989, 2009.
-    
+
     #   Awad H. Al-Mohy and Nicholas J. Higham, September 7, 2010.
-    
+
     t = 1; # Number of columns used by NORMEST1.
-    
+
     n = size(A,1);
 
     # function afun_power(flag,X)
     #     #AFUN_POWER  Function to evaluate matrix products needed by NORMEST1.
-    #     
+    #
     #     if flag == "dim"
     #         Z = n;
     #     elseif flag == "real"
@@ -28,29 +28,29 @@ function normAm(A,m)
     #         end
     #     else
     #         (p,q) = size(X);
-    #         if p != n, 
-    #             error("Dimension mismatch") 
+    #         if p != n,
+    #             error("Dimension mismatch")
     #         end
-    #         
+    #
     #         if flag=="notransp"
     #             for i = 1:m
-    #                 X = A*X; 
+    #                 X = A*X;
     #             end
     #         elseif flag=="transp"
     #             for i = 1:m
     #                 X = A'*X
     #             end
     #         end
-    #         
+    #
     #         Z = X;
-    #         
+    #
     #     end
-    #     
+    #
     #     return Z
     # end
 
 
-    if isequal(A,abs(A))
+    if isequal(A,abs.(A))
         e = ones(n,1);
         for j=1:m         # for positive matrices only
             e = A'*e;
@@ -63,8 +63,7 @@ function normAm(A,m)
         c = norm(A^m,1);
         mv = 6*t*m;
     end
-    
-    return (c,mv)
-    
-end
 
+    return (c,mv)
+
+end
