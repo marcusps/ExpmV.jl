b4d9eae7175f2252efbba9235d1df597403a3273
Matteo Rossi <teo.red90@gmail.com>
2018-05-04 11:33:01 +0300

    Fixed a problem with an `Int` variable that was supposed to be `Float`

--- src/expmv_tspan.jl
+++ src/expmv_tspan.jl
@@ -1,16 +1,8 @@
 function expmv(t::StepRangeLen, A, b; M = [], prec = "double", shift = true, full_term = false, prnt = false)
-              
     t0 = Float64(t.ref)
     tmax = Float64(t.ref + (t.len - 1.) * t.step)
-    q = t.len - 1 # For some reason
+    q = t.len - 1
     n = size(A, 1)
-
-    mu = 0.
-    if shift
-        mu = trace(A)/n
-        #mu = full(mu) # Much slower without the full!
-        A = A-mu*speye(n)
-    end
 
     force_estm = false; temp = (tmax -t0)*norm(A, 1);
     if (prec == "single" || prec == "half" && temp > 85.496) || ( prec == "double" && temp > 63.152)
@@ -18,10 +10,7 @@ function expmv(t::StepRangeLen, A, b; M = [], prec = "double", shift = true, ful
     end
 
     if isempty(M)
-        (M, mvd, alpha, unA) = select_taylor_degree(A, b, prec=prec, shift=shift, force_estm=force_estm)
-        mv = mvd
-    else
-        mv = 0
+        (M, alpha, unA) = select_taylor_degree(A, b, prec=prec, shift=shift, force_estm=force_estm)
     end
 
     tol =
@@ -40,13 +29,19 @@ function expmv(t::StepRangeLen, A, b; M = [], prec = "double", shift = true, ful
     temp, s = degree_selector(tmax - t0, M, U, p)
     h = (tmax - t0)/q;
 
-    X[:,1], mvnew = expmvi(t0,A,b,M=M,prec=prec,shift=shift,prnt=prnt);
-    mv += mvnew
+    X[:,1] = expmv(t0,A,b,M=M,prec=prec,shift=shift,prnt=prnt);
+
+    mu = 0.
+    if shift
+        mu = trace(A)/n
+        #mu = full(mu) # Much slower without the full!
+        A = A-mu*speye(n)
+    end
+
     d = Int(floor(q/s))
     j = Int(floor(q/d))
     r = q - d * j
     z = X[:,1]
-
     m_opt, = degree_selector(d, M, U, p)
     dr = d
     for i = 1:j+1
@@ -59,24 +54,21 @@ function expmv(t::StepRangeLen, A, b; M = [], prec = "double", shift = true, ful
         for k = 1:dr
             f = z
             c1 = norm(z, Inf)
+
             for p = 1:m_opt
                 if p > m
                     K[:,p+1] = (h/p)*(A*K[:,p])
-                    mv += 1
                 end
 
-                temp = (k^p)*K[:,p+1]
+                temp = (Float64(k)^p)*K[:,p+1]
                 f += temp
                 c2 = norm(temp, Inf)
-
                 if c1 + c2 <= tol * norm(f, Inf)
                     break
                 end
                 c1 = c2
             end
-
             m = max(m,p)
-
             X[:, k + (i - 1) * d + 1] = exp(k * h * mu) * f
         end
 
@@ -84,6 +76,5 @@ function expmv(t::StepRangeLen, A, b; M = [], prec = "double", shift = true, ful
             z = X[:,i*d+1]
         end
     end
-
     return X
 end
