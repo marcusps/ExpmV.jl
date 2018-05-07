8bdc87f660dd01774abd3751332e735dbf38746c
Matteo Rossi <teo.red90@gmail.com>
2018-05-07 16:05:45 +0300

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,30 @@
 using ExpmV
 using Base.Test
 
-for i = 1:20, d in 10:10:60, herm in [true, false]
-    r = sprandn(d,d,.1)+1im*sprandn(d,d,.1)
-    if herm
-        r = (r-r')/2
+@testset "Hermitian: $herm"  for herm in [true, false]
+    @testset "Size: $d" for d in 10:10:60
+        for i = 1:20
+            r = sprandn(d,d,.1)+1im*sprandn(d,d,.1)
+            if herm
+                r = (r-r')/2
+            end
+
+            rv = randn(d)+1im*randn(d)
+            rv = rv / norm(rv,2)
+
+            rt = randn()
+
+            x = expmv(rt,r,rv)
+            @testset "Against expm" begin
+                @test norm(x-expm(full(rt*r))*rv,2) ≈ 0.0 atol=1.0e-9
+            end
+            # Test the StepRangeLen version against the normal version
+            @testset "Timespan $nt timesteps" for nt in [5 11 51]
+                t = linspace(0, rt, nt)
+                @time x = expmv(t,r,rv)
+                @time y = hcat([expmv(ti,r,rv) for ti in t]...)
+                @test x ≈ y atol=1.0e-10
+            end
+        end
     end
-
-    rv = randn(d)+1im*randn(d)
-    rv = rv / norm(rv,2)
-
-    rt = randn()
-
-    x = expmv(rt,r,rv)
-    @test norm(x-expm(full(rt*r))*rv,2) ≈ 0.0 atol=1.0e-9
-
-    # Test the StepRangeLen version against the normal version
-
-    t = linspace(0, rt, 51)
-    x = expmv(t,r,rv)
-    y = hcat([expmv(ti,r,rv) for ti in t]...)
-    @test x ≈ y atol=1.0e-10
 end
